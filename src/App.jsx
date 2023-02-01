1173cae9be6e88c9f89db82a81e023a3bdde4b48
YashwantNG <gawandeyashwant7@gmail.com>
2023-02-01 15:10:17 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import React, { useState } from 'react'
-import questions from './data'
-import data from './data'
-import SingleQuestion from './Question'
+import React, { useState } from "react";
+import questions from "./data";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  // itrate over data
-  // display siglequestion
-  const [question,setQuestion] = useState(data)
+  const [question, setQuestion] = useState(data);
 
   return (
-    <main> 
-      <div className='container'>
+    <main>
+      <div className="container">
         <h3>question and answers about login</h3>
-        <section className='info'>
-          {questions.map((question) =>{
-            return <SingleQuestion key={question.id}{...question} />
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
           })}
         </section>
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
